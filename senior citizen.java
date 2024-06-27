ff6227751e929a02642c69d06514c9fde7af17e3
sridurgamandem <sreemandem@gmail.com>
2024-06-27 21:51:26 +0530

    Create senior citizen.java

---
+++ senior citizen.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        char G=s.next().charAt(0);
+        int N=s.nextInt();
+        if(G>=60)
+        {
+            System.out.println("Senior Citizen");
+        }
+        
+       else if(N>=58)
+        {
+            System.out.println("Senior Citizen");
+        }
+        
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
